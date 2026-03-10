07cbbe611829254ab2fde60fb69b10d30b4723b4
Gabriel Torres <gabrieltorres@G4F-N-LUCIANO.goias.intra>
2026-03-10 10:14:28 -0300

    feat: adding pages folder

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <h1>
+      Homens 🔥 queimem a vila 🏘️ e o templo 🏛️ de Atenas ⚔️, destruam 💥 tudo e
+      todos ☠️, vamos saudar 🙌 nosso senhor Ares 🩸🛡️, vamos homens ⚔️, acabem
+      com tudo 💣🔥!!
+    </h1>
+  );
+}
+
+export default Home;
